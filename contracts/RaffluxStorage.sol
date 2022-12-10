01d789e595f3aa9d988dfb291c81483402ed27b1
amaixin <earlvisbeatz@gmail.com>
2022-12-10 19:01:29 +0100

    New Contract and Test Update

---
+++ contracts/RaffluxStorage.sol
@@ -0,0 +1,59 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract RaffluxStorage {
+
+  
+       //Libs
+    using Counters for Counters.Counter;
+    Counters.Counter internal raffleIDsCounter;
+    Counters.Counter internal raffleMembersParticipants;
+    Counters.Counter internal raffleSellersParticipants;
+
+
+        //Global Vars
+    address owner;
+    uint internal minRaffleParticipationFee;
+    uint internal raffleSellersEntryFee = 0.02 ether;
+    address[] internal vipUsers;
+    uint internal housePointsSellers = 300;
+    uint internal housePointsPartpants = 500;
+
+
+
+    
+    //Raffle Proposal State 
+    enum ProposalSate {
+        Applied,
+        Waiting,
+        Approved,
+        Rejected
+    }
+
+    //Raffle Item
+    struct RaffleItem {
+        uint id;
+        address owner;
+        string uri;
+        string name;
+
+    }
+
+      //Raffle Sellers
+    struct AssetSeller {
+        address sellerAddress;
+        uint housePoints;
+        uint id;
+    }
+    
+      //Raffle Participants
+    struct RaffleParticipants {
+        address participantAddress;
+         uint housePoints;
+        uint _id;
+    }
+
+
+}
--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,48 @@
-const Lock = await hre.ethers.getContractFactory("Rafflux");
-const lock = await Lock.deploy();
+async function startRafflux() {
+  //deploy and create NFT from NFT contract
+  const [owner, randSigner] = await hre.ethers.getSigners();
+  const MyContract = await hre.ethers.getContractFactory("Maaket");
+  const deployedContract = await MyContract.deploy();
+  await deployedContract.deployed();
+
+  const nftContractAddress = await deployedContract.address;
+  console.log("NFT Contract Address :" + nftContractAddress);
+
+  //Create NFT
+  var cNft = await deployedContract.connect(randSigner);
+
+  await cNft.createNft("testingurl", hre.ethers.utils.parseEther("0.02"), {
+    value: hre.ethers.utils.parseEther("0.02"),
+  });
+
+  //Deploy Rafflux contracts
+  const rafContract = await hre.ethers.getContractFactory("Rafflux");
+  const rafDeployed = await rafContract.deploy();
+  await rafDeployed.deployed();
+  console.log("Deployed Contract Address :" + (await rafDeployed.address));
+
+  const contractbal = await hre.ethers.provider.getBalance(rafDeployed.address);
+
+  console.log("Contract Balance: ", hre.ethers.utils.formatEther(contractbal));
+
+  var rafSigner = await rafDeployed.connect(randSigner);
+  let transfrItem = await rafSigner.createRaffle(
+    1,
+    1,
+    nftContractAddress,
+    hre.ethers.utils.parseEther("0.02"),
+    { value: hre.ethers.utils.parseEther("0.02") }
+  );
+}
+
+async function runDeployer() {
+  try {
+    await startRafflux();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
 
 runDeployer();
