b82503fe2ebf47eaf02f70b2c8ea76574abac1e8
amaixin <earlvisbeatz@gmail.com>
2022-12-10 18:31:46 +0100

    e
    Contracts Update

---
+++ contracts/Maaket.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+
+contract Maaket is ERC721URIStorage {
+    address owner;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    Counters.Counter private _soldItems;
+    using SafeMath for uint;
+    
+
+    uint256 listingPrice = 0.02 ether;
+    
+
+    constructor() payable ERC721("Maaket", "MTK"){
+
+        owner = payable(msg.sender);
+    }
+
+    struct ItemData {
+        uint256 id;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool listed;
+    }
+
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Must be owner~");
+
+        _;
+    }
+
+
+    mapping(uint256 => ItemData) listedItem;
+
+
+    function updateListingPrice(uint256 _newPrice) public payable onlyOwner{
+        listingPrice = _newPrice;
+    
+    }
+
+    function getListingPrice() public view returns(uint256){
+        return listingPrice;
+    }
+
+    function getLatestListedToken() public view returns (ItemData memory) {
+
+        uint256 currentItem = _tokenIdCounter.current();
+        return listedItem[currentItem];
+    }
+
+
+    function getListedToken (uint256 _id) public view returns(ItemData memory){
+        return listedItem[_id];
+    }
+
+    function getCurrentTokenId () public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+
+    function createListedToken(uint256 id, uint256 price) private {
+        listedItem[id] = ItemData(id, payable(address(this)), payable(msg.sender), price,  true);
+        _transfer(msg.sender, address(this), id);
+
+    }
+
+    function createNft(string memory tokenURI, uint256 price) public payable returns (uint256){
+        require(msg.value == listingPrice, "Not enough ether to list" );
+        require(price > 0, "Price must not be negative" );
+        _tokenIdCounter.increment();
+        uint256 currentId = _tokenIdCounter.current();
+
+        _safeMint(msg.sender, currentId);
+        _setTokenURI(currentId, tokenURI);
+
+        createListedToken(currentId, price);
+
+        return currentId;
+
+
+    }
+
+
+    function getAllNfts() public view returns (ItemData[] memory){
+        uint currentCount = _tokenIdCounter.current();
+
+        ItemData[] memory allNfts = new ItemData[](currentCount);
+
+        uint currentIndex = 0;
+
+        for(uint i = 0; i < currentCount; i++ ){
+            uint currentId = i + 1;
+
+            ItemData storage currentItem = listedItem[currentId];
+            allNfts[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+
+        return allNfts;
+    }
+
+
+    function getMyNfts() public view returns(ItemData[] memory ){
+        uint totalitemCount = _tokenIdCounter.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+
+        for(uint i = 0; i < totalitemCount; i++){
+            if(listedItem[i+1].owner == msg.sender || listedItem[i+1].seller == msg.sender){
+                itemCount +=1;
+            }
+        }
+
+
+
+        ItemData[] memory nfts = new ItemData[](itemCount);
+
+        for(uint i = 0; i < totalitemCount; i++ ){
+             if(listedItem[i+1].owner == msg.sender || listedItem[i+1].seller == msg.sender){
+                uint currentId = i+1;
+                ItemData storage currentItem = listedItem[currentId];
+                nfts[currentIndex] = currentItem;
+                currentIndex +=1 ;
+            }
+        }
+
+        return nfts;
+    }
+
+
+    function executeNFTSale (uint256 _id) public payable{
+        uint price = listedItem[_id].price;
+        require(msg.value == price, "Insufficient funds");
+
+        address seller = listedItem[_id].seller;
+        listedItem[_id].listed = true;
+        listedItem[_id].seller = payable(msg.sender);
+        _soldItems.increment();
+
+        _transfer(address(this), msg.sender, _id);
+        approve(address(this), _id);
+
+        //transfer listing fee to contract who acts as owner
+        payable(owner).transfer(listingPrice);
+        //transfer NFT sale price of NFT to the item seller
+        payable(seller).transfer(msg.value);
+
+    }
+
+
+
+    // The following functions are overrides required by Solidity.
+
+
+}
---
+++ contracts/RaffluxStorage.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "hardhat/console.sol";
+import "./RaffluxStorage.sol";
+
+
+contract Rafflux is RaffluxStorage {
+  
+  address public erc721contractAddr;
+  IERC721 erc721contract = IERC721(erc721contractAddr);
+  IERC1155 erc1155contract = IERC1155(erc721contractAddr);
+
+    enum assetType {
+        ERC721,
+        ERC1155
+    }
+    //launcher 
+    constructor() payable{
+        owner = payable(msg.sender);
+    }
+
+    // receive() external payable{}
+
+
+    function _transferFromSeller(assetType _type, address _seller, uint256 _assetID) internal{
+    if (_type == assetType.ERC721) {
+      erc721contract.safeTransferFrom(_seller, address(this), _assetID);
+    } else if (_type == assetType.ERC1155) {
+      erc1155contract.safeTransferFrom(_seller, address(this), _assetID, 1, "");
+    }
+  }
+
+
+
+    function createRaffle(assetType _type, uint _id, address _contractAddr, uint _participateFee) public payable{
+        require(msg.value == raffleSellersEntryFee, "insufficient funds");
+        erc721contractAddr = _contractAddr;
+        minRaffleParticipationFee = _participateFee;
+        _transferFromSeller(_type, msg.sender, _id);
+    }
+
+    function transferBal( address payable _to, uint amt) public {
+         _to.transfer(amt);
+    }
+
+
+    function getBal(address _add) public view returns (uint){
+        return _add.balance;
+    }
+
+
+}
